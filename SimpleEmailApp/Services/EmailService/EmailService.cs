5d7506818b0cc65f280d5296ccb2ed319940caab
pyone <pyoneeisan@gmail.com>
2022-12-09 15:37:59 +0700

    Add project files.

---
+++ SimpleEmailApp/Services/EmailService/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace SimpleEmailApp.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void SendEmail(EmailDto emailDto)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailUserName").Value));
+            email.To.Add(MailboxAddress.Parse(emailDto.To));
+            email.Subject = emailDto.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = emailDto.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailUserName").Value, _configuration.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true); ;
+        }
+    }
+}
